5218ee2dc2dc9e81c67f61b38ab48a55ff6697ff
Ashley Kwon <hk216@duke.edu>
2022-11-10 19:17:47 -0500

    attempting to solve deployment problem

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import BakeryItem from "./components/BakeryItem" 
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
 
@@ -8,24 +9,45 @@ bakeryData.forEach((item) => {
 });
 /* ############################################################## */
 
+
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [cart, setCart] = useState([])
+  
+  function updateCart(item){
+    setCart([...cart, item])
+  }
 
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+       
+        <BakeryItem item={item} updateFunction={updateCart}/>
+        
       ))}
+     
 
       <div>
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        {
+        cart.map((item, index) => (
+          <div class = "CartItem">
+            <p>{item.name}</p>
+          </div>
+        ))}
+        {
+          <div class = "CartItem">
+             <p>{cart.map((item, index)=>(item.price))
+                      .reduce((prevSum, currentPrice) => prevSum + currentPrice, 0)}</p>
+          </div>
+       }
       </div>
     </div>
   );
 }
 
 export default App;
+
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,11 @@
 // TODO: create a component that displays a single bakery item
+export default function BakeryItem({item, updateFunction}){
+    return(
+    <div class = "BakeryItem"> 
+        <p>{item.name}</p>
+        <img src = {item.image}/>
+        <p>{item.price}</p>
+        <p>{item.description}</p>
+        <button onClick = {() => updateFunction(item)}>Add to Cart</button>
+    </div>)
+}
